27a2e9fdcb893cdda258faa65024eefc29859c44
iska000s <izabelast@op.pl>
2023-06-17 18:20:51 +0200

    Changes to js

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,16 +1,7 @@
-import React, {Component} from "react";
-import {createRoot} from "react-dom/client";
-import {figures} from "./figures";
-
-
-const container = document.getElementById("app");
-const root = createRoot(container);
-
-const App = () => {
-    return(
-        <>
-        </>
-    )
-}
-
-root.render(<App/>)
+// import React, {Component} from "react";
+// import {createRoot} from "react-dom/client";
+// import {figures} from "./figures";
+//
+//
+// const container = document.getElementById("app");
+// const root = createRoot(container);
--- dist/index.83119b98.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=index.83119b98.js.map
