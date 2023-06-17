a5a54875ef7c66872f5bb41870893bcd0e8270ca
iska000s <izabelast@op.pl>
2023-06-17 16:12:59 +0200

    Finished basic css

---
+++ Project/js/app.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {createRoot} from "react-dom/client";
+import {figures} from "./figures";
+
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+const App = () => {
+    return(
+        <>
+        </>
+    )
+}
+
+root.render(<App/>)
---
+++ Project/js/figures.js
@@ -0,0 +1,8 @@
+export const figures = [
+    {
+        id: 1,
+        img: "../img/img_main_page.jpeg",
+        category: '',
+        level: 'basic',
+    }
+]
